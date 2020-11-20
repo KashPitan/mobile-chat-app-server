b21d54c32e9bdda1cca03b79ac8b380425ba8d66
kashpitan <kashpitan@googlemail.com>
2020-11-20 12:30:20 +0000

    initial commit: small server created

---
+++ express.js
@@ -0,0 +1,16 @@
+const Express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+//sets up express app
+const App = Express();
+
+App.get((res, req, next) => {
+  res.send("SUCCESS");
+});
+
+//listen for requests on set up variable OR port 4000
+App.listen(process.env.port || 4000, () => {
+  console.log("listening on port 4000");
+});
