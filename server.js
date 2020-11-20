6bf2a04dc0755e6c9eee4a30b902f35fe579f403
kashpitan <kashpitan@googlemail.com>
2020-11-20 20:29:42 +0000

    fix: removed .ebextensions file and changed port to 8080

--- server.js
+++ server.js
@@ -3,14 +3,12 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-//sets up express app
 const App = Express();
 
 App.get("/", (req, res, next) => {
   res.send("SUCCESS");
 });
 
-//listen for requests on set up variable OR port 4000
-App.listen(process.env.port || 8081, () => {
-  console.log("listening on port 8081");
+App.listen(process.env.port || 8080, () => {
+  console.log("listening on port 8080");
 });
