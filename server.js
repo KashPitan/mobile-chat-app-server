397efa8b9dcf983f6ee6f525055da0d404d7530c
kashpitan <kashpitan@googlemail.com>
2020-11-21 02:36:18 +0000

    feat:added simple socket implementation: allows connection and message sending

--- server.js
+++ server.js
@@ -3,12 +3,33 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+const Socket = require("socket.io");
+
 const App = Express();
 
+const messages = [];
+
 App.get("/", (req, res, next) => {
-  res.send("SUCCESS");
+  console.log("success");
+  res.json({ text: "Success" });
 });
 
-App.listen(process.env.port || 8080, () => {
+App.get("/messages", (req, res, next) => {
+  res.send(messages);
+});
+
+const Server = App.listen(process.env.port || 8080, () => {
   console.log("listening on port 8080");
+});
+
+const IO = Socket(Server);
+
+IO.on("connection", function (socket) {
+  console.log("socket connection made", socket.id);
+
+  socket.on("chat", function (data) {
+    messages.push(data);
+    console.log("chatting", data);
+    IO.sockets.emit("chat", data);
+  });
 });
